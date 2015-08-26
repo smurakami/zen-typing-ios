0d211cfe5e0b644a3b1f7e0e0ab7bfda8994fcda
s.murakami <smurakami.84@gmail.com>
2015-08-26 18:52:19 +0900

    Initial Commit

---
+++ ZenTyping/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ZenTyping
+//
+//  Created by 村上晋太郎 on 2015/08/26.
+//  Copyright (c) 2015年 村上晋太郎. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
